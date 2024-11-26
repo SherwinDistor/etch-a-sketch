0ae75d8f3762ff0064868a589139f72051e77f3e
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-11-26 10:41:33 -0600

    Pseudocode for grid creation

---
+++ scrpit.js
@@ -0,0 +1,9 @@
+// Grab grid-container from document
+
+// Create a function that takes in size and creates divs
+
+    // Create a while loop that will run 256 times to create a 16 x 16 grid
+        // Use createElement to create a div with the reference variable square
+        // Take the reference and set .classList.add(square)
+        // parentNode.appendChild() to append as the last child of the parentNode
+//
